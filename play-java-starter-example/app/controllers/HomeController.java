d961ddc1a1f2a7738343d64577907cb273886cc7
nattapong59 <nattapong.sa.59@ubu.ac.th>
2018-10-27 21:33:21 +0700

    new

--- play-java-starter-example/app/controllers/HomeController.java
+++ play-java-starter-example/app/controllers/HomeController.java
@@ -12,22 +12,22 @@ public class HomeController extends Controller {
     public Result booking(){
         return ok(views.html.booking.render());
     }
-    public Result test(){
-        return ok(views.html.test.render());
-    }
     public Result check(){
         return ok(views.html.check.render());
     }
     public Result rules(){
         return ok(views.html.rules.render());
     }
-    public Result MedicalData(){
-        return ok(views.html.MedicalData.render());
+    public Result medical_data(){
+        return ok(views.html.medical_data.render());
     }
-    public Result Employee(){
-        return ok(views.html.Employee.render());
+    public Result management_employee(){
+        return ok(views.html.management_employee.render());
     }
-    public Result MMM() {
-        return ok(views.html.Managementroom.render());
+    public Result management_room() {
+        return ok(views.html.management_room.render());
+   }
+   public Result payment(){
+       return ok(views.html.payment.render());
    }
 }
