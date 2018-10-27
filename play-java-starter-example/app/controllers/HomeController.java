8e912f6bb2b18211ffde48d97a6f798b816c281f
nattapong59 <nattapong.sa.59@ubu.ac.th>
2018-10-27 15:37:57 +0700

    nick up

--- play-java-starter-example/app/controllers/HomeController.java
+++ play-java-starter-example/app/controllers/HomeController.java
@@ -15,35 +15,31 @@ public class HomeController extends Controller {
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
-     */
-<<<<<<< HEAD
-    public Result main() {
-        return ok(views.html.main.render());
-    }
+     */ 
+public Result main() {
+    return ok(views.html.main.render());
+}
 
-    public Result booking(){
-        return ok(views.html.booking.render());
-    }
-    public Result test(){
-        return ok(views.html.test.render());
-    }
-    public Result check(){
-        return ok(views.html.check.render());
-    }
+public Result booking(){
+    return ok(views.html.booking.render());
+}
+public Result test(){
+    return ok(views.html.test.render());
+}
+public Result check(){
+    return ok(views.html.check.render());
+}
 }
 =======
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    public Result MedicalData(){
-        return ok(views.html.MedicalData.render());
-    }
-    public Result Employee(){
-        return ok(views.html.Employee.render());
-    }
-    
-
+public Result index() {
+    return ok(index.render("Your new application is ready."));
+}
+public Result MedicalData(){
+    return ok(views.html.MedicalData.render());
+}
+public Result Employee(){
+    return ok(views.html.Employee.render());
 }
 
 
->>>>>>> c1ae5811d6f85c87b86f877326f884c8b9ad24f7
+}
